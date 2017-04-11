18e025316af4605936ed7830e1bd94001a734090
Matej Janeček <matej.janecek19@gmail.com>
2017-04-11 09:22:47 +0200

    Format edit

--- js/upload.js
+++ js/upload.js
@@ -28,7 +28,6 @@ function fileUploader(options){
 				counter = 0;
 			}
 		});
-		this.options.dropHandler.css('background', 'black');
 		this.options.dropHandler.on('dragenter', function (e){
 			e.stopPropagation();
 			e.preventDefault();
@@ -71,7 +70,9 @@ function fileUploader(options){
 		file.trigger('click');
 	});
 	file.change(function(){
-		for (var i = 0; i < $(this).get(0).files.length; ++i) {
+		// console.log($(this).get(0).files);
+		for (var i = 0; i < $(this).get(0).files.length; i++) {
+			// console.log($(this).get(0).files[i]);
 			var formdata = new FormData();
 			formdata.append(_this.options.fileInputName, $(this).get(0).files[i]);
 			// console.log(formdata);
@@ -80,7 +81,8 @@ function fileUploader(options){
 	}); 
 	
 	this.upload = function(formdata){
-		var upload_progress = new this.options.progressObject(formdata.getAll("file")[0]);
+		// console.log(formdata.getAll(this.options.fileInputName)[0]);
+		var upload_progress = new this.options.progressObject(formdata.getAll(this.options.fileInputName)[0]);
 		upload_progress.ajax = $.ajax({
 			url: './upload.php',
 			type: 'POST',
@@ -121,53 +123,3 @@ function fileUploader(options){
 		});
 	}
 }
-$(function(){
-	fileUploader({
-		dropHandler: $("#handler"),
-		fileInputName: "file",
-		multiple: true,
-		action: "./upload.php",
-		onDragEnter: function(){
-			//css
-			this.dropHandler.css("background", "red");
-		},
-		onDragOver: function(){
-			//css
-			this.dropHandler.css("background", "green");
-		},
-		onDragOut: function(){
-			//css
-			this.dropHandler.css("background", "black");
-		},
-		progressObject: function fileProgress(file_info){
-			this.file_info = file_info;
-			this.progress = $("<div></div>");
-			this.progress.css({
-				width: 0,
-				height: 100,
-				"background-size": "cover",
-				"background-color": "red"
-			});
-			this.progress.appendTo($("body"));
-			if($.inArray(file_info.type, ["image/gif", "image/jpeg", "image/png"]) > -1){
-				var reader = new FileReader();
-				reader.onload = (function(obj){
-					return function(e){
-						obj.progress.css("background-image", "url("+this.result+")");
-					};
-				})(this);
-				reader.readAsDataURL(file_info);
-			}
-			
-			this.update = function(percent, upload_progress){
-				this.progress.css("width", percent + "px");
-				this.progress.html(Math.round(upload_progress.time_remaining) + "s" + Math.round(upload_progress.mbps*100)/100 + " <br>" +this.file_info.name + " " + Math.round(percent*100)/100 + "%");
-			}
-			this.progress.on("click", {upload_progress: this}, function(e){
-				e.data.upload_progress.ajax.abort();
-				e.data.upload_progress.progress.remove();
-			});
-		}
-	});
-});
-	
